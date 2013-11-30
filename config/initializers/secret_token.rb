88328801f7209e15a20d1608e7ea09db03ca6f5b
tibet000000 <Brian.D.P.Joseff@dartmouth.edu>
2013-11-30 15:27:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeopleAndStuff::Application.config.secret_key_base = '0086ed364a25bfc112deb69e5852ae6fe732ee1c172bf040f7cef624c221e5394dd14d20ea006e7342895d8e0817a02df1b42a4a35fbec2f1bb7df7176dfd588'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PeopleAndStuff::Application.config.session_store :cookie_store, key: '_people-and-stuff_session'
